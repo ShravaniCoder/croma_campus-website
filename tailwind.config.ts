c08abd8afb91ce74369740d871e8c8261d71f4c7
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-03-19 19:03:44 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "15px",
+      },
+      colors: {
+        customColor: "#063455",
+      },
+      fontFamily: {
+        sans: ["var(--font-poppins)"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
