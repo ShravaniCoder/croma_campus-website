3b83c4aeedb66df31599d28be99511cd4a897180
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-03-20 17:51:05 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from "next/image";
 import Logo from "../../public/logo.png";
-import Phone from "../../public/phone.png";
-import Menu from "../../public/menu.png";
-import Profile from "../../public/profile.png";
-import Options from "../../public/options.png";
+import { BiSolidPhoneCall } from 'react-icons/bi';
+import { BiMenu } from 'react-icons/bi';
+import { CgProfile } from 'react-icons/cg';
+import { BsTable } from 'react-icons/bs';
 import { CiSearch } from "react-icons/ci";
+import { BsWhatsapp } from 'react-icons/bs';
+
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
+  const [slide, setSlide] = useState(false)
   return (
-    
-    
-      <div className="bg-customColor">
+    <>
+      <section className="md:hidden lg:hidden">
+        <div className="bg-green-500 p-2 text-white font-bold text-center">
+          <div className="flex justify-center items-center gap-2">
+            <BsWhatsapp className="text-2xl" />
+            <p className="text-2xl">GUIDE ME</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-customColor">
         <div className="flex w-full items-center justify-between px-[20px] py-[10px] lg:container lg:mx-auto lg:px-20">
           <div className="flex gap-x-4">
-            <Image src={Menu} alt="Menu Button" className="lg:hidden" />
+            <BiMenu
+              className="text-3xl text-white lg:hidden"
+              onClick={() => setShow(!show)}
+            />
             <Image
               src={Logo}
               alt="Logo"
@@ -23,17 +39,21 @@ const Navbar = () => {
               className="hidden lg:block"
             />
             <div className="hidden border-r-2 border-white h-10 lg:block"></div>
-            <button className="items-center justify-center border-opacity-10 text-white rounded-md hidden py-2 px-5 lg:block border lg:flex">
-              <Image src={Options} alt="Options" className="mr-1" />
+            <button
+              className="items-center justify-center border-opacity-10 text-white rounded-md hidden py-2 px-5 lg:block border lg:flex"
+              onClick={() => setSlide(!slide)}
+            >
+              <BsTable className="mr-1" />
               Course Menu
             </button>
             <div className="flex rounded">
               <input
                 type="text"
-                className="text-1xl rounded-l-md px-3 min-w-[350px] hidden lg:block"
-                placeholder="Search your course here!"
+                className="text-sm rounded-l-md p-1 min-w-[350px] border-none hidden lg:block"
+                placeholder="Search your course here !"
+                onClick={() => setSlide(!slide)}
               />
-              <CiSearch className="bg-orange-500 py-2 px-2 justify-center min-w-10 min-h-11 hidden lg:block rounded-r-md" />
+              <CiSearch className="bg-orange-500 text-white py-2 px-2 justify-center min-w-10 min-h-11 hidden lg:block rounded-r-md" />
             </div>
           </div>
 
@@ -48,22 +68,60 @@ const Navbar = () => {
           </div>
 
           <div className="flex items-center gap-x-2">
-            <Image src={Phone} alt="Call" className="rounded lg:hidden" />
-            <span className="hidden font-medium text-white lg:block">
+            <div className="bg-orange-500 rounded-md lg:hidden">
+              <BiSolidPhoneCall className="text-4xl p-1 text-white" />
+            </div>
+
+            <span className="hidden text-sm font-medium text-white lg:block">
               All Courses
             </span>
             <div className="hidden border-r-4 border-white h-6 lg:block"></div>
-            <span className="hidden font-medium text-white lg:block">
+            <span className="hidden text-sm font-medium text-white lg:block">
               Corporate Training
             </span>
-            <Image
-              src={Profile}
-              alt="Profile"
-              className="hidden lg:block ml-4"
-            />
+            <CgProfile className="hidden text-3xl text-white lg:block ml-4" />
           </div>
         </div>
-      </div>
+      </section>
+
+      {show && (
+        <div className="lg:hidden w-full p-2 h-[180px] bg-black rounded-sm">
+          <div className="flex justify-center items-center bg-black mt-5">
+            <button className="px-4 py-1 gap-1 font-bold text-white border border-white-500 rounded-lg flex justify-center items-center">
+              <BsTable className="mr-1" />
+              <p>Course Menu</p>
+            </button>
+          </div>
+          <div className="flex justify-center items-center mt-4">
+            <input
+              type="text"
+              placeholder="Search your course here !"
+              className="p-1 rounded-sm min-w-[300px] outline-none"
+            />
+            <CiSearch className="bg-orange-500 text-white py-2 px-2 justify-center min-w-8 min-h-8 lg:block rounded-r-md" />
+          </div>
+          <div className="All-course flex justify-center items-center gap-2 text-center text-white mt-4">
+            <span className="text-sm font-medium text-white lg:block">
+              All Courses
+            </span>
+            <span className="text-xl h-8">|</span>
+            <span className="text-sm font-medium text-white lg:block">
+              Corporate Training
+            </span>
+            <CgProfile className="text-3xl text-white lg:block ml-1" />
+          </div>
+        </div>
+      )}
+      {slide && (
+        <div>
+          
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Navbar;
     
   );
 }
